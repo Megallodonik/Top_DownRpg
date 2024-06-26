cdead953d2a343cf7438ed67c75822d2415d9aa7
Megallodonik <megallodonik@gmail.com>
2024-06-26 16:00:12 +0300

    Tree cutting

---
+++ Assets/Script/MainCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, 0.01f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     NavMeshAgent agent;
     Vector2 mousePos;
     private Camera cam;
+    bool TreeNear = false;
+    GameObject TreeNearObj = null;
     void Start()
     {
         cam = Camera.main;
@@ -25,6 +27,29 @@ public class Player : MonoBehaviour
             Debug.Log("кнопка");
             agent.SetDestination(mousePos);
             
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0) && TreeNear)
+        {
+            Debug.Log("Tree deleted");
+            Destroy(TreeNearObj);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("trigger");
+        if (other.gameObject.CompareTag("Tree"))
+        {
+            Debug.Log("tree");
+            TreeNear = true;
+            TreeNearObj = other.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (TreeNear)
+        {
+            TreeNear = false;
+            TreeNearObj = null;
         }
     }
 }
